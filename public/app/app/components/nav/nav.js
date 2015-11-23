54fa2a0e7d018743f0dc91e70ec874fba59ed2cc
nicolasfujita <nickfujita@gmail.com>
2015-11-22 20:10:15 -0800

    adding in playlist logic

--- public/app/app/components/nav/nav.js
+++ public/app/app/components/nav/nav.js
@@ -1,7 +1,20 @@
 'use strict';
 
-angular.module('nav', [])
+angular.module('nav', ['playlist'])
 
-.controller('NavController', function ($scope) {
-  
+.controller('NavController', function ($scope, $mdDialog) {
+  $scope.showPlaylist = function(ev) {
+    $mdDialog.show({
+      controller: 'PlaylistController',
+      templateUrl: '../views/playlist.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:true
+    })
+    .then(function(answer) {
+      $scope.status = 'You said the information was "' + answer + '".';
+    }, function() {
+      $scope.status = 'You cancelled the dialog.';
+    });
+  };
 });
